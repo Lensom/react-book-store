4cfa8b3abba2ef75b8fe456340c7be79817305de
Lensom <magurawork@gmail.com>
2020-09-06 18:02:27 +0300

    Start new project. Add route, redux

---
+++ src/services/bookstore-service.js
@@ -0,0 +1,16 @@
+export default class BookstoreService {
+  getBooks() {
+    return [
+      {
+        id: 1,
+        title: "Prod",
+        author: "Susan",
+      },
+      {
+        id: 2,
+        title: "Prod 2",
+        author: "Susan 2",
+      },
+    ];
+  }
+}
