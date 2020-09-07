9a87a9fd83c4985fda37744aafa21572ffb05fab
Lensom <magurawork@gmail.com>
2020-09-07 22:24:45 +0300

    Add store, css, items

--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -1,16 +1,28 @@
 export default class BookstoreService {
+  data = [
+    {
+      id: 1,
+      title: "Production-Ready Microservices",
+      author: "Susan J. Fowler",
+      price: 32,
+      coverImage:
+        "https://images-na.ssl-images-amazon.com/images/I/41yJ75gpV-L._SX381_BO1,204,203,200_.jpg",
+    },
+    {
+      id: 2,
+      title: "Release It!",
+      author: "Michael T. Nygard",
+      price: 45,
+      coverImage:
+        "https://images-na.ssl-images-amazon.com/images/I/414CRjLjwgL._SX403_BO1,204,203,200_.jpg",
+    },
+  ];
+
   getBooks() {
-    return [
-      {
-        id: 1,
-        title: "Prod",
-        author: "Susan",
-      },
-      {
-        id: 2,
-        title: "Prod 2",
-        author: "Susan 2",
-      },
-    ];
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(this.data);
+      }, 700);
+    });
   }
 }
